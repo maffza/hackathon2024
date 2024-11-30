43340f350a6f32b3c1793161458c847535109f36
Kozi <kuba.kozienski2203@gmail.com>
2024-11-30 13:55:36 +0100

    Menu template

---
+++ Breakthrough/Assets/Scenes/KozienskiScene/Scripts/ButtonController.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class OptionsButtonHandler : MonoBehaviour {
+    public void OnOptionsButtonClicked() {
+        Debug.Log("Options button clicked!");
+    }
+}
---
+++ Breakthrough/Assets/Scenes/KozienskiScene/Scripts/ButtonUI.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ButtonUI : MonoBehaviour
+{
+    public void OnOptionsButtonClicked() {
+        Debug.Log("Options button clicked!");
+    }
+}
